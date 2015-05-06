6eb30a7affa2ea89b6fa4e62e0f557b01ee9adb7
Andrew Toms <andrew@anglersatlas.com>
2015-05-06 14:54:45 -0700

    Created our first Datbase Migration. Our wittow server's growing up

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -5,15 +5,12 @@ use yii\db\Migration;
 
 class m130524_201442_init extends Migration
 {
-    public function up()
+    public function safeUp()
     {
-        $tableOptions = null;
-        if ($this->db->driverName === 'mysql') {
-            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-        }
 
-        $this->createTable('{{%user}}', [
+        $this->execute("CREATE SCHEMA test");
+
+        $this->createTable('test.user', [
             'id' => Schema::TYPE_PK,
             'username' => Schema::TYPE_STRING . ' NOT NULL',
             'auth_key' => Schema::TYPE_STRING . '(32) NOT NULL',
@@ -24,11 +21,31 @@ class m130524_201442_init extends Migration
             'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
             'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
             'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
-        ], $tableOptions);
+        ]);
+
+        $this->createTable("test.post", [
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . " NOT NULL",
+            'body' => Schema::TYPE_STRING . " NOT NULL",
+            'photo' => Schema::TYPE_STRING,
+            'author' => Schema::TYPE_INTEGER . " NOT NULL",
+            'time_created' => Schema::TYPE_TIMESTAMP . " DEFAULT NOW()",
+        ]);
+        $this->addForeignKey("post_author_fkey", "test.post", "author", "test.user", "id");
+
+        $this->createTable("test.user_liked_posts", [
+            'user_id' => Schema::TYPE_INTEGER,
+            'post_id' => Schema::TYPE_INTEGER,
+            'time_liked' => Schema::TYPE_TIMESTAMP . " DEFAULT NOW()",
+        ]);
+        $this->addPrimaryKey("user_liked_posts_pkey", "test.user_liked_posts", "user_id, post_id");
     }
 
-    public function down()
+    public function safeDown()
     {
-        $this->dropTable('{{%user}}');
+        $this->dropTable('test.user_liked_posts');
+        $this->dropTable('test.post');
+        $this->dropTable('test.user');
+        $this->execute('DROP SCHEMA test');
     }
 }
